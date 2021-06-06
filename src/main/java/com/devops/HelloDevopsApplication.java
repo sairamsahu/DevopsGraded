15b07ab96b5278cdbf815be7dcdb4912ab439ae1
sairamsahu <sairamsahu181@gmail.com>
2021-06-06 15:57:46 +0530

    first commit

---
+++ src/main/java/com/devops/HelloDevopsApplication.java
@@ -0,0 +1,13 @@
+package com.devops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloDevopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloDevopsApplication.class, args);
+	}
+
+}
